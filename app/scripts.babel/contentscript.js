0ca00e56ba07b36fefe07abf50a121317dfb7a5e
elrosti <elrosti@gmail.com>
2015-11-08 10:59:24 -0300

    - New Icon! - Small typo fix

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,7 +3,7 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
       var photos=document.getElementsByClassName('image-thumb');
       for(var i=0;i<photos.length;i++){
       	//photos[i].style.display='none';
-      	photos[i].innerHTML = '<div><p> Image has been bloqued </p></div>'
+      	photos[i].innerHTML = '<div><p> Image has been blocked </p></div>'
       }
    }
 });
